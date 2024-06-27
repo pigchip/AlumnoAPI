1a34bffe5a7f4d97ffb670e42a589148b9ce48ca
pigchip <guzmancruzandresmiguel@gmail.com>
2024-06-27 02:35:25 -0600

    last

--- src/main/java/com/ipn/mx/AlumnoAPI/config/WebConfig.java
+++ src/main/java/com/ipn/mx/AlumnoAPI/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://taupe-nougat-8f03f6.netlify.app")
+                .allowedOrigins("https://667d0efa50c3bc82818725da--taupe-nougat-8f03f6.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
